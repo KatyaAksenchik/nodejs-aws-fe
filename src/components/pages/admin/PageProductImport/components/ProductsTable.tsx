6ef7ac041d1f93acab0ddcd4fd60cfb944fdd8ea
Ekaterina Aksenchik <ekaterina.aksenchik@credentially.io>
2020-11-08 20:49:03 +0300

    Task 4: Updated request for create product request

--- src/components/pages/admin/PageProductImport/components/ProductsTable.tsx
+++ src/components/pages/admin/PageProductImport/components/ProductsTable.tsx
@@ -16,14 +16,14 @@ export default function ProductsTable() {
   const [products, setProducts] = useState<any>([]);
 
   useEffect(() => {
-    axios.get(`${API_PATHS.bff}/product`)
+    axios.get(`${API_PATHS.bff}/books`)
       .then(res => setProducts(res.data));
   }, []);
 
   const onDelete = (id: string) => {
-    axios.delete(`${API_PATHS.bff}/product/${id}`)
+    axios.delete(`${API_PATHS.bff}/book/${id}`)
       .then(() => {
-        axios.get(`${API_PATHS.bff}/product`)
+        axios.get(`${API_PATHS.bff}/book`)
           .then(res => setProducts(res.data));
         }
       );
