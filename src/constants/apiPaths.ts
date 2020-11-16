1d979d873e304653ddc9fe8a5a6ee9bd1b6c4bef
Ekaterina Aksenchik <ekaterina.aksenchik@credentially.io>
2020-11-16 14:44:45 +0300

    Task 5: Import to s3

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://eedpvgz07g.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://eedpvgz07g.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://eedpvgz07g.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://t9snpcchii.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://eedpvgz07g.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
